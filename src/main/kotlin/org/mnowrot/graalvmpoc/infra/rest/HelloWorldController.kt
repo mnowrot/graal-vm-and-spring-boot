816354e7a6180f70da4d5c4bbab14db08ff3b5a9
Marcin Nowrot <marcin.nowrot@gmail.com>
2025-10-03 18:48:16 +0200

    Add Kotlin Logging, configure Logback, and update `HelloWorldController` for logging support

--- src/main/kotlin/org/mnowrot/graalvmpoc/infra/rest/HelloWorldController.kt
+++ src/main/kotlin/org/mnowrot/graalvmpoc/infra/rest/HelloWorldController.kt
@@ -1,5 +1,6 @@
 package org.mnowrot.graalvmpoc.infra.rest
 
+import io.github.oshai.kotlinlogging.KotlinLogging
 import org.mnowrot.graalvmpoc.config.AppConfig
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
@@ -7,8 +8,13 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class HelloWorldController(private val config: AppConfig) {
 
+    companion object {
+        val log = KotlinLogging.logger {}
+    }
+
     @GetMapping("/")
     fun home(): String {
+        log.info { "GET / called. Message to be returned: ${config.message}" }
         return "${config.message}\n"
     }
 }
